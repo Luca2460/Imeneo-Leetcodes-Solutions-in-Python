2eb962bd6f9ddf7e7e6f3f3783a6e9890d8a380c
Luca2460 <64102952+Luca2460@users.noreply.github.com>
2021-08-19 22:37:57 +0200

    more edge cases added (negative integers)

--- 7. Reverse integer.py
+++ 7. Reverse integer.py
@@ -2,19 +2,23 @@ import math
 
 def solution(x):
     rev = 0
-    for i in range(math.floor(math.log10(x + 0.1))+1): # x is supposed to be int, so adding 0.1 will not change the ammount of digits it has
+    sign = 1
+    if x < 0:
+        sign = -1
+        x = abs(x)
+    for i in range(math.floor(math.log10(abs(x) + 0.1))+1): # x is supposed to be int, so adding 0.1 will not change the ammount of digits it has
         # pop                                          # adding an if statement for x = 0 would have worked too
         pop = x % 10
         x = int((x - pop) / 10)
         
         # push
         rev = 10*rev + pop
-    return rev
+    return rev*sign
 
     # TIME complexity: O(log(N)), where N is the input integer.
     # Explaination: either as N has roughly log(N) digits or as the for cycle iterates over roughly log(x) elements
 
     # SPACE complexity: O(1), the number of allocated variables is constant no matter the input
 
-x= 134
+x= -134
 print(solution(x))
