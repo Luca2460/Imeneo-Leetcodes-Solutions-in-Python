113908534307fddf97d5e79ea1e77bbf99f72952
Luca2460 <64102952+Luca2460@users.noreply.github.com>
2021-09-23 14:36:34 +0100

    10 Sep-Daily exercise 2/2

---
+++ 118. Pascal's Triangle.py
@@ -0,0 +1,16 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        res = []
+        
+        for i in range(1, numRows + 1):
+            temp = []
+            for j in range(i):
+                if j == 0 or j == i-1:
+                    temp.append(1)
+                else:
+                    temp.append(res[i-2][j-1]+res[i-2][j])
+            res.append(temp)
+            
+        return res
+        # TIME complexity: O(N^2) it's the size of the output
+        # SPACE complexity: O(N^2) it's the size of the output
